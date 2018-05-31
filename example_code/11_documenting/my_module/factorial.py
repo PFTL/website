aa9f7cfed9d4b1fc15e6a81f06deaa8dbed97beb
Aquiles Carattino <aquiles@aquicarattino.com>
2018-05-31 09:07:14 +0200

    Base structure for documenting

---
+++ example_code/11_documenting/my_module/factorial.py
@@ -0,0 +1,15 @@
+import math
+
+def factorial(n):
+    if not n >= 0:
+        raise ValueError("n must be >= 0")
+    if math.floor(n) != n:
+        raise ValueError("n must be exact integer")
+    if n + 1 == n:  # catch a value like 1e300
+        raise OverflowError("n too large")
+    result = 1
+    factor = 2
+    while factor <= n:
+        result *= factor
+        factor += 1
+    return result
