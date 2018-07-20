a9b2a9b384991d5fa41ac852a5c384fbc252d993
Aquiles Carattino <aquiles@aquicarattino.com>
2018-07-20 17:15:38 +0200

    Solved issue with newsletter sign up not working

--- theme/static/js/newsletter_subscribe.js
+++ theme/static/js/newsletter_subscribe.js
@@ -22,6 +22,61 @@ $(document).ready(function () {
         },
         error: function () {
           $("#newsletter_feedback").html('<div class="alert alert-danger" role="alert">An error ocurred, please try again!</div>');
+          $("#send").prop("disabled", false);
+        }
+       });
+  });
+  $('#newsletter-sidebar').submit(function (event) {
+    event.preventDefault();
+
+    var email = $("#email-sidebar").val();
+    $.ajax({
+        type: "POST",
+        url: "https://api.elasticemail.com/v2/contact/add",
+        data: {
+          'publicAccountID': '7616a022-4e37-40ba-b90e-6c3735d8440e',
+          'email': email,
+          'listName': 'PFTL Newsletter',
+          'activationTemplate': '',
+          'source': 'WebForm',
+          'ewf_captcha': 'false'},
+        beforeSend: function () {
+          $("#send-sidebar").prop("disabled", true);
+          $("#send-sidebar").text("Sending");
+        },
+        success: function (data) {
+          $("#newsletter_feedback-sidebar").html('<div class="alert alert-primary" role="alert">Thanks for signing up to our newsletter!</div>');
+        },
+        error: function () {
+          $("#newsletter_feedback-sidebar").html('<div class="alert alert-danger" role="alert">An error ocurred, please try again!</div>');
+          $("#send-sidebar").prop("disabled", false);
+        }
+       });
+  });
+  $('#newsletter-popup').submit(function (event) {
+    event.preventDefault();
+
+    var email = $("#email-popup").val();
+    $.ajax({
+        type: "POST",
+        url: "https://api.elasticemail.com/v2/contact/add",
+        data: {
+          'publicAccountID': '7616a022-4e37-40ba-b90e-6c3735d8440e',
+          'email': email,
+          'listName': 'PFTL Newsletter',
+          'activationTemplate': '',
+          'source': 'WebForm',
+          'ewf_captcha': 'false'},
+        beforeSend: function () {
+          $("#send-popup").prop("disabled", true);
+          $("#send-popup").text("Sending");
+        },
+        success: function (data) {
+          $("#newsletter_feedback-popup").html('<div class="alert alert-primary" role="alert">Thanks for signing up to our newsletter!</div>');
+        },
+        error: function () {
+          $("#newsletter_feedback-popup").html('<div class="alert alert-danger" role="alert">An error ocurred, please try again!</div>');
+          $("#send-popup").prop("disabled", false);
         }
        });
   });
