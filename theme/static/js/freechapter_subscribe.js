f05d1620579ccf0f5d15ce26e82e35c6cc1b9603
Aquiles Carattino <aquiles@aquicarattino.com>
2019-01-18 10:29:11 +0100

    Using custom free chapter request

--- theme/static/js/freechapter_subscribe.js
+++ theme/static/js/freechapter_subscribe.js
@@ -2,17 +2,13 @@ $(document).ready(function () {
   $('#free-chapter').submit(function (event) {
     event.preventDefault();
 
-    var email = $("#email").val();
+    var email = {'email': $("#email").val()};
     $.ajax({
         type: "POST",
-        url: "https://api.elasticemail.com/v2/contact/add",
-        data: {
-          'publicAccountID': '7616a022-4e37-40ba-b90e-6c3735d8440e',
-          'email': email,
-          'listName': 'Free PFTL Chapter',
-          'activationTemplate': '',
-          'source': 'WebForm',
-          'ewf_captcha': 'false'},
+        dataType: 'json',
+        contentType: 'application/json',
+        url: "http://uetke.uetke.com/free_chapter",
+        data: JSON.stringify(email),
         beforeSend: function () {
           $("#send").prop("disabled", true);
           $("#send").text("Sending");
