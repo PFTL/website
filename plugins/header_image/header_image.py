a0a69b5501e4df14a605c8709f8009f28694f1cd
Aquiles Carattino <aquiles@uetke.com>
2019-10-04 16:10:51 +0200

    Improved plugin and theme to deal with illustrations

--- plugins/header_image/header_image.py
+++ plugins/header_image/header_image.py
@@ -26,6 +26,7 @@ th_sizes = {
     'twitter': [800, 418],
     'facebook': [476, 249],
     'linkedin': [1200, 627],
+    'header': [825, 1200]
 }
 
 logger = logging.getLogger(__name__)
@@ -38,17 +39,27 @@ font_path_website = os.path.join(cur_dir, 'IndieFlower.ttf')
 def attach_clipper(x):
     return x[9:] if x[9] == '/' else x[8:]
 
+
 def filename_clipper(x):
     return x[11:] if x[11] == '/' else x[10:]
 
 
+def illustration_clipper(x):
+    return x[15:] if x[15] == '/' else x[14:]
+
+
 def process_image(generator, content, image):
+    illustration = False
     if image.startswith('{attach}'):
         image = attach_clipper(image)
         path = os.path.join(generator.settings.get('PATH'), content.relative_dir, image)
     elif image.startswith('{filename}'):
         image = filename_clipper(image)
         path = os.path.join(generator.settings.get('PATH'), image)
+    elif image.startswith('{illustration}'):
+        illustration = True
+        image = illustration_clipper(image)
+        path = os.path.join(generator.settings.get('PATH'), content.relative_dir, image)
     else:
         logger.error('Header Image: Tag not recognized: {}'.format(image))
         return
@@ -77,64 +88,72 @@ def process_image(generator, content, image):
             im.save(output_image_path)
 
         for key in th_sizes:
-            th_name = ''.join(image.split('.')[:-1]) + '_' + key + '.jpg'
+            th_name = ''.join(image.split('.')[:-1]) + '_' + key + '.' + image.split('.')[-1]
             th_full_path = os.path.join(output_path, th_name)
             logger.info('Save image path: {}'.format(th_full_path))
 
             th_size = th_sizes[key]
 
-            if not os.path.isfile(th_full_path) \
-                    or generator.settings.get('FORCE_IMG_REBUILD', False):
+            if not os.path.isfile(th_full_path) or generator.settings.get('FORCE_IMG_REBUILD', False):
                 im = Image.open(output_image_path)
-                width, height = im.size
-
-                #  Compare the aspect ratios
-                ar_image = width/height
-                ar_th =th_size[0]/th_size[1]
-
-                if ar_image > ar_th:
-                    width_th = width * th_size[1] / height
-                    im.thumbnail((width_th, th_size[1]), Image.ANTIALIAS)
-                    # Crop the image to the desired size, assuming the height is correct
-                    left = int((width_th - th_size[0]) / 2)
-                    right = int((width_th + th_size[0]) / 2)
-                    im_copped = im.crop((left, 0, right, th_size[1] - 1))
-
+                if key is 'header':
+                    im.thumbnail((th_size[0], th_size[1]), Image.ANTIALIAS)
+                    im_copped = im
                 else:
-                    height_th = height * th_size[0]/width
-                    im.thumbnail((th_size[0], height_th), Image.ANTIALIAS)
-                    # Crop the image to the desired size, assuming the width is correct
-                    bottom = int((height_th - th_size[1]) / 2)
-                    top = int((height_th + th_size[1]) / 2)
-                    im_copped = im.crop((0, bottom, th_size[0], top))
+                    width, height = im.size
+                    #  Compare the aspect ratios
+                    ar_image = width/height
+                    ar_th =th_size[0]/th_size[1]
 
-                # Make it darker, to display the text without problems
-                brightness = ImageEnhance.Brightness(im_copped)
-                im_dark = brightness.enhance(0.5)
-                draw = ImageDraw.Draw(im_dark)
+                    if ar_image > ar_th:
+                        width_th = width * th_size[1] / height
+                        im.thumbnail((width_th, th_size[1]), Image.ANTIALIAS)
+                        # Crop the image to the desired size, assuming the height is correct
+                        left = int((width_th - th_size[0]) / 2)
+                        right = int((width_th + th_size[0]) / 2)
+                        im_copped = im.crop((left, 0, right, th_size[1] - 1))
 
-                # Using a font size equivalent to 1/5 of the height
-                font_size = int(th_size[1] / 3.5 / 10 * 7.5)  #  The last bit: /10*7.5 is to convert to points from pixels
-                font = ImageFont.truetype(font_path_title, font_size)
-                text = textwrap.fill(title, width=24)
-                text_size = draw.textsize(text)
-                logger.info('Text size: {}, image width: {}'.format(text_size, th_size[0]))
-                y_pos = int(th_size[1]*1/10)
-                x_pos = int(th_size[0]/8)
-                draw.text((x_pos, y_pos), text, (255, 255, 255), font=font)
+                    else:
+                        height_th = height * th_size[0]/width
+                        im.thumbnail((th_size[0], height_th), Image.ANTIALIAS)
+                        # Crop the image to the desired size, assuming the width is correct
+                        bottom = int((height_th - th_size[1]) / 2)
+                        top = int((height_th + th_size[1]) / 2)
+                        im_copped = im.crop((0, bottom, th_size[0], top))
 
-                # Write the website name
-                font = ImageFont.truetype(font_path_website, int(font_size*.8))
-                text = textwrap.fill("Python for the Lab.com", width=24)
-                text_size = draw.textsize(text)
-                logger.info('Website text size: {}, image width: {}'.format(text_size, th_size[0]))
-                x_pos = int(th_size[0] / 9)
-                y_pos = int(th_size[1] * 3.8 / 5)
-                draw.text((x_pos, y_pos), text, (255, 255, 255), font=font)
+                if illustration:
+                    im_copped.save(th_full_path)
+                else:
+                    # Make it darker, to display the text without problems
+                    brightness = ImageEnhance.Brightness(im_copped)
+                    im_dark = brightness.enhance(0.5)
+                    draw = ImageDraw.Draw(im_dark)
 
-                im_dark.save(th_full_path)
+                    # Using a font size equivalent to 1/5 of the height
+                    font_size = int(th_size[1] / 3.5 / 10 * 7.5)  #  The last bit: /10*7.5 is to convert to points from pixels
+                    font = ImageFont.truetype(font_path_title, font_size)
+                    text = textwrap.fill(title, width=24)
+                    text_size = draw.textsize(text)
+                    logger.info('Text size: {}, image width: {}'.format(text_size, th_size[0]))
+                    y_pos = int(th_size[1]*1/10)
+                    x_pos = int(th_size[0]/8)
+                    draw.text((x_pos, y_pos), text, (255, 255, 255), font=font)
+
+                    # Write the website name
+                    font = ImageFont.truetype(font_path_website, int(font_size*.8))
+                    text = textwrap.fill("Python for the Lab.com", width=24)
+                    text_size = draw.textsize(text)
+                    logger.info('Website text size: {}, image width: {}'.format(text_size, th_size[0]))
+                    x_pos = int(th_size[0] / 9)
+                    y_pos = int(th_size[1] * 3.8 / 5)
+                    draw.text((x_pos, y_pos), text, (255, 255, 255), font=font)
+                    im_dark.save(th_full_path)
 
             setattr(content, 'header_'+key, os.path.join(out_dir, th_name))
+
+        if illustration:
+            th_name = ''.join(image.split('.')[:-1]) + '_' + 'header' + '.' + image.split('.')[-1]
+            setattr(content, 'illustration', os.path.join(out_dir, th_name))
 
     else:
         logger.error('photo: No photo for {} at {}'.format(content.source_path, path))
