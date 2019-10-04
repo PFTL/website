14544b6e8fa39ce66d080949d7d91d1f6eca84ba
Aquiles Carattino <aquiles@uetke.com>
2019-10-04 16:09:49 +0200

    Final version of the example code of imports

--- example_code/37_imports/AF_relative/mod_b/file_b.py
+++ example_code/37_imports/AF_relative/mod_b/file_b.py
@@ -1,5 +1,5 @@
 from ..mod_a.file_a import function_a
-from mod_a.file_c import function_c
+from AF_relative.mod_b.mod_a.file_c import function_c
 
 def function_b():
     print('This is function_b')
